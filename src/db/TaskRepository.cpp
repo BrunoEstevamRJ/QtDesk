f64656d57e99483fb8d2efe8a6af4b5d697cc790
Bruno Estevam <bruno.estevam@outlook.com.br>
2026-04-05 18:43:04 -0300

    TasksRepository (Break)

---
+++ src/db/TaskRepository.cpp
@@ -0,0 +1,43 @@
+#include "TaskRepository.h"
+#include "Database.h"
+#include <QSqlQuery>
+#include <QSqlRecord>
+
+TaskRepository::TaskRepository()
+{
+    db = Database::connect();
+}
+
+void TaskRepository::createTable()
+{
+    QSqlQuery query(db);
+
+    query.exec("CREATE TABLE IF NOT EXISTS tasks ("
+               "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+               "title TEXT)");
+}
+
+void TaskRepository::addTask(const QString& title)
+{
+    QSqlQuery query(db);
+
+    query.prepare("INSERT INTO tasks (title) VALUES (:title)");
+    query.bindValue(":title", title);
+    query.exec();
+}
+
+std::vector<Task> TaskRepository::getTasks()
+{
+    std::vector<Task> tasks;
+
+    QSqlQuery query("SELECT id, title FROM tasks", db);
+
+    while (query.next()) {
+        Task t;
+        t.id = query.value("id").toInt();
+        t.title = query.value("title").toString();
+        tasks.push_back(t);
+    }
+
+    return tasks;
+}
---
+++ src/db/TaskRepository.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <QSqlDatabase>
+#include <QString>
+#include <vector>
+#include "../core/Task.h"
+
+class TaskRepository {
+private:
+    QSqlDatabase db;
+
+public:
+    TaskRepository();
+
+    void createTable();
+    void addTask(const QString& title);
+    std::vector<Task> getTasks();
+};
